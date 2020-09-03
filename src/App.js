2ddb5f526e26b534f12fd7a3d6169fb2569bee6e
JPDent <jdelaney.ent@outlook.com>
2020-09-02 21:04:56 -0700

    added more content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from "./Logov3.png";
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Owned & Operated by <span style={{ fontWeight: "bold" }}>TANYA D. MARTIN</span></p>
+        <p className="headerLocation">Richmond, VA</p>
+        <p className="headerEmail">Email: tdmartin.notarypublic@gmail.com</p>
+        <p> </p>
       </header>
     </div>
   );
