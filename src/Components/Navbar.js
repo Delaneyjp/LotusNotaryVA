1011dcddda44c2537013cee32e3fa761264539b2
JPDent <jdelaney.ent@outlook.com>
2020-12-06 00:21:16 -0800

    bootstrap'd nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,12 +12,12 @@ class Navbar extends Component {
         }
 
         return (
-            <nav className="navbar sticky-top navbar-expand-sm" style={{ height: "60px" }}>
-                <div className="container-fluid">
-                    <ul className=" container navbar-nav">
+            <nav className="navbar sticky-top navbar-expand-sm row" style={{ height: "60px" }}>
+                <div className="container-fluid ">
+                    <ul className=" container navbar-nav col-3">
                         <li className="nav-item "><a href="#TitleSlide" className="nav-link tabHome"><img src={navlogo} className="App-nav-logo" alt="navlogo" style={navlogostyle} /></a></li>
                     </ul>
-                    <ul className="container navbar-nav ml-auto">
+                    <ul className="container navbar-nav col-9">
                         <li className="nav-item "> <a href="#AboutMeSlide" className="tabAbout navTab nav-link">ABOUT ME</a></li>
                         <li className="nav-item"> <a href="#MessageMeSlide" className="tabMessage navTab nav-link">CONTACT</a></li>
                         <li className="nav-item"> <a className="nav-link"><span style={{ float: "left" }}><RequestNavButton /> </span><span className="tabReq navTab">Request Quote</span></a></li>
